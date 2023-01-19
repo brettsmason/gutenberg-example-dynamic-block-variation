0e3f9002a2d8b503dca54ec83ec8d3380e363a3f
Brett Mason <brett@orphans.co.uk>
2023-01-19 08:31:13 +0000

    Working example of dynamic block with variations

--- build/template.php
+++ build/template.php
@@ -1,3 +1,7 @@
+<?php
+$is_variation = $attributes['isVariation'] ? 'This is a variation' : 'This is not a variation';
+?>
+
 <p <?php echo get_block_wrapper_attributes(); ?>>
- <?php esc_html_e( 'Example Dynamic – hello from a dynamic block!', 'example-dynamic' ); ?>
+	<?php printf( '%1$s. I have %2$s columns', esc_html( $is_variation ), esc_attr( $attributes['columns'] ) ); ?>
 </p>
